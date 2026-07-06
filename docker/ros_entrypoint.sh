0b57ff13b1e14920c5b49f46cb5ab6382b58ccad
Wiktor <wiktorwszedybyl1@gmail.com>
2026-07-06 19:19:06 +0200

    Initial commit

---
+++ docker/ros_entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/jazzy/setup.bash
+source /opt/franka_ws/install/setup.bash
+
+if [ -f /ws/install/setup.bash ]; then
+    source /ws/install/setup.bash
+fi
+
+exec "$@"
